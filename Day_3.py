e7d136e94fe3a5835e84aa95e68d9d237cf735b0
AiphatShaban <140347172+AiphatShaban@users.noreply.github.com>
2024-01-17 21:54:20 +0300

    new changes

--- Day_3.py
+++ Day_3.py
@@ -1,15 +1,32 @@
 print("Welcome to the rollercoaster!")
 height = int(input("Enter your height in cm:  "))
 
+bill = 0 
+
 if height >= 120:
     print("You can ride the rollercoaster! ")
     age = int(input("what is your age? "))
     if age < 12:
-        print("Please pay $5.")
+        bill =5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")    
+        bill = 12
+        print("Adult tickets are $12.") 
+
+    wants_photos = input("Do you want a photo taken? Y or N :  ")
+    if wants_photos == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")    
+
+
+
+
+
+
 else:
     print("Sorry, you have to grow taller before you can ride the rollercoaster.")    
 
--- Day_3_3.py
+++ Day_3_3.py
@@ -1,2 +1,15 @@
 # leap year checker
 year = int(input("Which year would you like to check?  "))
+
+
+if  year %4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("leap year")
+        else:
+            print("Not leap")
+    else:
+        print("leap year")
+else:
+    print("Not leap year")    
+    
